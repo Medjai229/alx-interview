2eb679579fc7043336cfc7056b25e65b27f4f5d3
Malik <malikhussein229@gmail.com>
2024-08-15 22:16:32 +0300

    Task#0 Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
+"""
+Log Parser Module
+
+This module parses log data from standard input and prints
+the file size and occurrence count of each status code every 10 lines.
+
+The module consists of two functions: `print_status` and `main`.
+"""
 import sys
 
 
 def print_status(size, status):
-    
+    """
+    Prints the size of a file and the occurrence count of each status code.
+    """
     print(f'File size: {size}')
 
-    for code, count in status.items(): # sorted
+    for code, count in status.items():  # sorted
         if count > 0:
-            print (f'{code}: {count}')
-
-
-
-
-
-
-
+            print(f'{code}: {count}')
 
 
 def main():
+    """
+    The main function of the program, responsible
+    for parsing log data from standard input.
+    """
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                     '403': 0, '404': 0, '405': 0, '500': 0}
     size = 0
@@ -27,7 +34,7 @@ def main():
         for line in sys.stdin:
             split_line = line.split()
             if count % 10 == 0 and count != 0:
-                print_status(size=size, status= status_codes)
+                print_status(size=size, status=status_codes)
             try:
                 code = split_line[-2]
                 if code in status_codes.keys():
@@ -39,10 +46,11 @@ def main():
             except Exception:
                 pass
             count += 1
-        print_status(size=size, status= status_codes)
+        print_status(size=size, status=status_codes)
     except KeyboardInterrupt:
-        print_status(size=size, status= status_codes)
+        print_status(size=size, status=status_codes)
         raise
+
 
 if __name__ == "__main__":
     main()
