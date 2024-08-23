487ecc91461ad01cfe1c0f3b503858670d7caa7c
Malik <malikhussein229@gmail.com>
2024-08-23 23:28:10 +0300

    Task#0 pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+0-validate_utf8.py
+
+This module provides a function to check if a given list of integers represents a valid UTF-8 encoding.
+
+Author: Malik Hussein
+"""
 
 def validUTF8(data):
+    """
+    Checks if a given list of integers represents a valid UTF-8 encoding.
+    """
 
     number_of_bytes = 0
 
@@ -9,7 +19,7 @@ def validUTF8(data):
 
     for i in data:
 
-        mask_n_bytes = 1 << 7 
+        mask_n_bytes = 1 << 7
 
         if number_of_bytes == 0:
 
@@ -22,14 +32,14 @@ def validUTF8(data):
 
             if number_of_bytes == 1 or number_of_bytes > 4:
                 return False
-            
+
         else:
             if not (i & mask_1 and not (i & mask_2)):
                 return False
-            
+
         number_of_bytes -= 1
 
     if number_of_bytes == 0:
         return True
-    
+
     return False
