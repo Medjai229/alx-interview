3b5afdcfdbe95809fc3e46cb3b77268a9c7ef6bb
Malik <malikhussein229@gmail.com>
2024-09-18 13:04:38 +0300

    Task#0 Back to the greedy method for the time efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,11 +12,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    num_coins = 0
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        while total >= coin:
+            total -= coin
+            num_coins += 1
 
-    return dp[total] if dp[total] != float('inf') else -1
+    if total == 0:
+        return num_coins
+    else:
+        return -1
