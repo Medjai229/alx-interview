3449d5d0764a878b058990ddc255611eb516ad8e
Malik <malikhussein229@gmail.com>
2024-09-18 12:41:26 +0300

    Task#0 Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,6 +9,9 @@ def makeChange(coins, total):
     Returns the fewest number of coins that you need to make change for total
     with the coins. If total is 0, or if you can't make change, returns -1
     """
+    if total <= 0:
+        return 0
+
     coins.sort(reverse=True)
     num_coins = 0
 
