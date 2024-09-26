ee0a8f51240d4f54594a0684f2706bdfcfd6d1c0
Malik <malikhussein229@gmail.com>
2024-09-26 20:44:21 +0300

    Task#0 Using DFS method

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,24 +4,42 @@ Module to find the island perimeter
 """
 
 
-def island_perimeter(grid):
+def dfs(grid, i, j):
     """
-    Calculates the perimeter of an island in a grid.
+    Perform DFS to calculate the perimeter of the island.
     """
-    height = len(grid)
-    width = len(grid[0])
+    # If the current cell is out of bounds or water
+    if i < 0 or i > len(grid) or j < 0 or j > len(grid[0]) or grid[i][j] == 0:
+        return 1
+
+    # If the cell is visited before return 0
+    if grid[i][j] == -1:
+        return 0
+
+    # Mark the visited cell
+    grid[i][j] = -1
+
+    # Start the perimeter
     perimeter = 0
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j]:
-                # Start with 4 for each land cell
-                perimeter += 4
-
-                # Subtract 2 for each adjacent land cell
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+    # Explore the neighboring cells
+    perimeter += dfs(grid, i - 1, j)  # UP
+    perimeter += dfs(grid, i + 1, j)  # DOWN
+    perimeter += dfs(grid, i, j - 1)  # LEFT
+    perimeter += dfs(grid, i, j + 1)  # RIGHT
 
     return perimeter
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island using DFS.
+    """
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                # Start DFS from the first land cell
+                return dfs(grid, i, j)
+
+    # If no land was found
+    return 0
